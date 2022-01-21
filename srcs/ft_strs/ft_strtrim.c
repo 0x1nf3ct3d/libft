707ff797384d6ddaa6f829faeee90f4657ef772f
Pingu1n <86953591+hypn0x@users.noreply.github.com>
2022-01-21 13:53:15 +0100

    Update ft_strtrim.c

--- srcs/ft_strs/ft_strtrim.c
+++ srcs/ft_strs/ft_strtrim.c
@@ -12,6 +12,7 @@
 
 #include "libft.h"
 
+/*
 static int	ft_strcmp(char c, char const *set)
 {
 	size_t	i;
@@ -49,4 +50,24 @@ char	*ft_strtrim(char const *s1, char const *set)
 		ptr[i++] = s1[start++];
 	ptr[i] = '\0';
 	return (ptr);
+}
+*/
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	char	*res;
+	size_t	len;
+
+	if (!s1)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len > 0 && ft_strchr(set, s1[len - 1]))
+		len--;
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, s1, len + 1);
+	return (res);
 }
